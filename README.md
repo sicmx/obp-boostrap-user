98bc533e84ff39498c79df51a802558a8f29cead
chrisjsimpson <chris@karmacomputing.co.uk>
2019-03-28 15:06:24 +0000

    boostrap an open bank project user

---
+++ README.md
@@ -0,0 +1,22 @@
+# Auto create initial Open Bank Project user
+
+Useful for automating deployment of open bank project; can
+be used to bootstrap an initial user in a controlled way.
+
+- Uses python 3
+- Boostrap an Open Bank Project user automatically
+
+## Requirements
+
+```
+pip install -r requirements.txt
+```
+
+## Run
+
+- First, edit `.env` with desired account info (double check password policy, else with fail on insecure / invalid password)
+
+```
+source .env # otherwise with read from environmet (e.g Kubernetes controlled)
+python main.py # Register new user
+```
---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import subprocess
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.firefox.options import Options
+import time 
+
+# Get geko driver if not present
+if os.path.isfile('./geckodriver') is False:
+  subprocess.run("./get-geckodriver.sh")
+  
+
+OBP_HOST = os.getenv('OBP_HOST', False)
+OBP_PASSWORD = os.getenv('OBP_PASSWORD', False)
+OBP_USERNAME = os.getenv('OBP_USERNAME', False)
+OBP_EMAIL = os.getenv('OBP_EMAIL', False)
+OBP_FIRSTNAME = os.getenv('OBP_FIRSTNAME', False)
+OBP_LASENAME = os.getenv('OBP_LASENAME', False)
+
+options = Options()
+options.headless = False
+
+fp = webdriver.FirefoxProfile()
+
+browser = webdriver.Firefox(firefox_profile=fp, options=options, executable_path='./geckodriver')
+
+browser.get(OBP_HOST + '/user_mgt/sign_up');
+
+firstnameElm = browser.find_element_by_id('txtFirstName')
+lastnameElm = browser.find_element_by_id('txtLastName')
+emailElm = browser.find_element_by_id('txtEmail')
+usernameElm = browser.find_element_by_id('txtUsername')
+passwordElms = browser.find_elements_by_css_selector('input[type="password"]')
+password1Elm = passwordElms[0]
+password2Elm = passwordElms[1]
+
+## Fill form
+firstnameElm.send_keys(OBP_FIRSTNAME)
+lastnameElm.send_keys(OBP_LASENAME)
+emailElm.send_keys(OBP_EMAIL)
+usernameElm.send_keys(OBP_USERNAME)
+password1Elm.clear()
+password1Elm.send_keys(OBP_PASSWORD)
+password2Elm.clear()
+password2Elm.send_keys(OBP_PASSWORD)
+## Submit form
+password2Elm.send_keys(Keys.RETURN)
