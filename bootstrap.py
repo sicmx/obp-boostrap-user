6ce0675fa4fa7eb6c9fc050ae843ed71e6f1e121
chrisjsimpson <chris@karmacomputing.co.uk>
2019-03-31 13:53:26 +0100

    refactored create_consumer and create_new_user

---
+++ bootstrap.py
@@ -0,0 +1,25 @@
+import os
+from create_new_user import create_new_user
+from create_consumer import create_consumer
+from obp_python import getAuthToken, getUserId
+
+# Register new user
+create_new_user()
+
+# Create consumer (to generate consumer key & secret)
+create_consumer()
+
+# Get auth token
+auth_token = getAuthToken(CONSUMER_KEY=os.environ['CONSUMER_KEY'], OBP_ENDPOINT=os.environ['OBP_HOST'], OBP_USERNAME=os.environ['OBP_USERNAME'], PASSWORD=os.environ['OBP_PASSWORD'])
+
+# Get user id
+userId = getUserId(OBP_AUTH_TOKEN=auth_token,OBP_ENDPOINT=os.environ['OBP_HOST'])
+
+# Write user id to file
+with open('obp_user_id.txt', 'w') as fp:
+  fp.write(userId)
+
+
+# Inject super user id into kubernetest secret
+
+# Delete obpapi-service, redeploy new secret config map with superuser id 
--- create_consumer.py
+++ create_consumer.py
@@ -6,60 +6,66 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.firefox.options import Options
 import time 
 
-# Get geko driver if not present
-if os.path.isfile('./geckodriver') is False:
-  subprocess.run("./get-geckodriver.sh")
-  
+def create_consumer():
+  # Get geko driver if not present
+  if os.path.isfile('./geckodriver') is False:
+    subprocess.run("./get-geckodriver.sh")
+    
 
-OBP_HOST = os.getenv('OBP_HOST', False)
-OBP_PASSWORD = os.getenv('OBP_PASSWORD', False)
-OBP_USERNAME = os.getenv('OBP_USERNAME', False)
-OBP_EMAIL = os.getenv('OBP_EMAIL', False)
-OBP_FIRSTNAME = os.getenv('OBP_FIRSTNAME', False)
-OBP_LASENAME = os.getenv('OBP_LASENAME', False)
+  OBP_HOST = os.getenv('OBP_HOST', False)
+  OBP_PASSWORD = os.getenv('OBP_PASSWORD', False)
+  OBP_USERNAME = os.getenv('OBP_USERNAME', False)
+  OBP_EMAIL = os.getenv('OBP_EMAIL', False)
+  OBP_FIRSTNAME = os.getenv('OBP_FIRSTNAME', False)
+  OBP_LASENAME = os.getenv('OBP_LASENAME', False)
 
-options = Options()
-options.headless = False
+  options = Options()
+  options.headless = False
 
-fp = webdriver.FirefoxProfile()
+  fp = webdriver.FirefoxProfile()
 
-browser = webdriver.Firefox(firefox_profile=fp, options=options, executable_path='./geckodriver')
+  browser = webdriver.Firefox(firefox_profile=fp, options=options, executable_path='./geckodriver')
 
-# Login using credentials
-browser.get(OBP_HOST + '/user_mgt/login');
+  # Login using credentials
+  browser.get(OBP_HOST + '/user_mgt/login');
 
-usernameElm = browser.find_element_by_id('username')
-passwordElm = browser.find_element_by_id('password')
-## Fill in login form
-usernameElm.send_keys(OBP_USERNAME)
-passwordElm.send_keys(OBP_PASSWORD)
-## Submit form (login)
-passwordElm.send_keys(Keys.RETURN)
+  usernameElm = browser.find_element_by_id('username')
+  passwordElm = browser.find_element_by_id('password')
+  ## Fill in login form
+  usernameElm.send_keys(OBP_USERNAME)
+  passwordElm.send_keys(OBP_PASSWORD)
+  ## Submit form (login)
+  passwordElm.send_keys(Keys.RETURN)
 
-time.sleep(2)
-# Create new consumer
-browser.get(OBP_HOST + '/consumer-registration');
-id="appName"
-appNameElm = browser.find_element_by_id('appName')
-redirectUrlElm = browser.find_element_by_id('appRedirectUrl')
-emailElm = browser.find_element_by_id('appDev')
-descElm = browser.find_element_by_id('appDesc')
-# Fill in new consumer form
-appNameElm.send_keys('bootstrap')
-emailElm.send_keys(OBP_EMAIL)
-descElm.send_keys('...')
-time.sleep(2)
-#Submit
-emailElm.send_keys(Keys.RETURN)
-time.sleep(2)
+  time.sleep(2)
+  # Create new consumer
+  browser.get(OBP_HOST + '/consumer-registration');
+  id="appName"
+  appNameElm = browser.find_element_by_id('appName')
+  redirectUrlElm = browser.find_element_by_id('appRedirectUrl')
+  emailElm = browser.find_element_by_id('appDev')
+  descElm = browser.find_element_by_id('appDesc')
+  # Fill in new consumer form
+  appNameElm.send_keys('bootstrap')
+  emailElm.send_keys(OBP_EMAIL)
+  descElm.send_keys('...')
+  time.sleep(2)
+  #Submit
+  emailElm.send_keys(Keys.RETURN)
+  time.sleep(2)
 
-# Extract auth key and 
-consumerKey = browser.find_element_by_id('auth-key').text
-consumerSecret = browser.find_element_by_id('secret-key').text
+  # Extract auth key and 
+  consumerKey = browser.find_element_by_id('auth-key').text
+  consumerSecret = browser.find_element_by_id('secret-key').text
 
-with open('consumerKey.txt', 'w') as fp:
-  fp.write(consumerKey)
-with open('consumerSecret.txt', 'w') as fp:
-  fp.write(consumerSecret)
+  os.environ['CONSUMER_KEY'] = consumerKey
 
-browser.quit()
+  with open('consumerKey.txt', 'w') as fp:
+    fp.write(consumerKey)
+  with open('consumerSecret.txt', 'w') as fp:
+    fp.write(consumerSecret)
+
+  browser.quit()
+
+if __name__ == '__main__':
+  create_consumer()
