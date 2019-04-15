df1f98914ebff4a8d3e472e777cfda1984e88506
chrisjsimpson <chris@karmacomputing.co.uk>
2019-04-15 18:41:08 +0100

    updated bootstrap.py to consume Response objects from obp python because it now returns the Response object, leaving the client to parse the response. TODO error handling

--- bootstrap.py
+++ bootstrap.py
@@ -2,6 +2,7 @@ import os
 from create_new_user import create_new_user
 from create_consumer import create_consumer
 from obp_python import getAuthToken, getUserId
+import json
 
 # Register new user
 create_new_user()
@@ -10,11 +11,15 @@ create_new_user()
 create_consumer()
 
 # Get auth token
-auth_token = getAuthToken(OBP_CONSUMER_KEY=os.environ['OBP_CONSUMER_KEY'], OBP_API_HOST=os.environ['OBP_API_HOST'], OBP_USERNAME=os.environ['OBP_USERNAME'], OBP_PASSWORD=os.environ['OBP_PASSWORD'])
+req = getAuthToken(OBP_CONSUMER_KEY=os.environ['OBP_CONSUMER_KEY'], OBP_API_HOST=os.environ['OBP_API_HOST'], OBP_USERNAME=os.environ['OBP_USERNAME'], OBP_PASSWORD=os.environ['OBP_PASSWORD'])
+
+auth_token = json.loads(req.text)['token']
+
+os.environ['OBP_AUTH_TOKEN'] = auth_token
 
 # Get user id
-userId = getUserId(OBP_AUTH_TOKEN=auth_token,OBP_API_HOST=os.environ['OBP_API_HOST'])
-
+req = getUserId()
+userId = json.loads(req.text)['user_id']
 # Write user id to file
 with open('obp_user_id.txt', 'w') as fp:
   fp.write(userId)
